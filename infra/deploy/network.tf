f3a70176d4ae4129c8fc7190560c1ac79366ceb0
shivam1610 <shivam16oct1996@gmail.com>
2025-10-06 17:10:40 +0530

    Corrected network

--- infra/deploy/network.tf
+++ infra/deploy/network.tf
@@ -27,7 +27,7 @@ resource "aws_subnet" "public_a" {
   vpc_id                  = aws_vpc.main.id
   cidr_block              = "10.1.1.0/24"
   map_public_ip_on_launch = true
-  availability_zone       = "${data.aws_region.current}a"
+  availability_zone       = "${data.aws_region.current.name}a"
   tags = {
     Name = "${local.prefix}-public-a"
   }
