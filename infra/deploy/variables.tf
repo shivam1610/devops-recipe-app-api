396a5b467f300004a1e2c38ae52313d94385b0a0
shivam1610 <shivam16oct1996@gmail.com>
2025-10-07 16:19:53 +0530

    added rds instance

--- infra/deploy/variables.tf
+++ infra/deploy/variables.tf
@@ -15,3 +15,13 @@ variable "contact" {
   default     = "devops-team@example.com"
 
 }
+
+variable "db_username" {
+  description = "Username for the recipe app api database"
+  default     = "recipeapp"
+}
+
+variable "db_password" {
+  description = "Password for the terraform database"
+
+}
