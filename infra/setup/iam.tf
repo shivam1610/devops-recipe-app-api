05e16e056129ae8c0c7b5cb711783d1d832f2dfe
shivam1610 <shivam16oct1996@gmail.com>
2025-10-08 13:16:17 +0530

    permission corrected 2

--- infra/setup/iam.tf
+++ infra/setup/iam.tf
@@ -175,7 +175,7 @@ data "aws_iam_policy_document" "rds" {
       "rds:CreateDBInstance",
       "rds:DeleteDBInstance",
       "rds:ListTagsForResource",
-      # "rds:AddTagsToResource", 
+      "rds:AddTagsToResource", 
       "rds:ModifyDBInstance"
     ]
     resources = ["*"]
